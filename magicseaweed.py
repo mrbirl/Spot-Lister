8997062a59697116a4ef5754c344cb9cab489302
Cian Brassil <mrb@live.ie>
2017-05-29 22:07:40 +1000

    Magicseaweed updated to handle unicode characters correctly

--- magicseaweed.py
+++ magicseaweed.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import io
 
 results = {}
 
@@ -7,7 +8,9 @@ results = {}
 for region_id in range(1, 13):
 
 	region_req_url = ' http://magicseaweed.com/api/mdkey/continent/%d?depth=4' % region_id
-	region_response = json.loads(requests.get(region_req_url).text)
+	response = requests.get(region_req_url)
+	response = requests.utils.get_unicode_from_response(response)
+	region_response = json.loads(response, encoding='utf-8')
 
 	region_name = region_response[0]['name']  # It's a dict in a list
 
@@ -34,7 +37,7 @@ for region_id in range(1, 13):
 	if area_results:
 		results[region_name] = area_results
 
-with open('magicseaweed_spots.json', 'w') as wf:
-	json.dump(results, wf, sort_keys=True, indent=4)
+with io.open("magicseaweed_spots.json",'w', encoding="utf-8") as outfile:
+  outfile.write(unicode(json.dumps(results, ensure_ascii=False, indent=4)))
 
 print 'Done'
